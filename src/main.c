43e7316667b2d90be388132727a9ea06c8d41aa2
Tenyaeva Ekaterina <b08lsoai@gmail.com>
2026-05-15 22:05:01 +0300

    feat: add args parser

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "algo/process_image.h"
+
+args_t *args = NULL;
+
+int main(int argc, char **argv) {
+  args = (args_t *)calloc(1, sizeof(args_t));
+  if (!args) {
+    fprintf(stderr, "Error: Cannot allocate args structure.\n");
+    return -1;
+  }
+
+  int ret = parse_args(argc, argv, args);
+  if (ret < 0) {
+    free_args(args);
+    return -1;
+  }
+
+  if (args->help_flag) {
+    free_args(args);
+    return 0;
+  }
+
+  ret = process_image(args);
+
+  if (ret < 0) {
+    free_args(args);
+    return -1;
+  }
+  free_args(args);
+  return 0;
+}
---
+++ src/utils/parser_args.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef enum {
+  INVALID = -1,
+  BLUR,
+  MOTION_BLUR,
+  GAUSSIAN_BLUR,
+  FIND_EDGES,
+  EMBOSS,
+  SHARPEN,
+} filter_name;
+
+typedef enum {
+  MODE_INVALID = -1,
+  MODE_SEQ,
+} conv_mode;
+
+typedef struct {
+  char **filenames;
+  size_t images_number;
+  conv_mode mode;
+  filter_name filter;
+  bool help_flag;
+} args_t;
+
+int parse_args(int argc, char *argv[], args_t *args);
+void free_args(args_t *args);
